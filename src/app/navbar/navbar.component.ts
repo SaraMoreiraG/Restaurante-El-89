874e7838011e0152764ab9340c38103b35e89159
SaraMoreiraG <sara_moreira@hotmail.es>
2023-08-01 11:16:58 +0200

    responsive navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'navbar',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  showMenu = false;
+  isSmallScreen = window.innerWidth <= 992;
 
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isSmallScreen = event.target.innerWidth <= 992;
+    // Hide the menu when resizing from large to small screen
+    if (this.isSmallScreen) {
+      this.showMenu = false;
+    }
+  }
+
+  constructor() {
+    // Trigger the initial check for screen size
+    this.onResize({ target: { innerWidth: window.innerWidth } });
+  }
 }
