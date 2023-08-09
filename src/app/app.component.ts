c421696ca0bcce23c584e3ebe11acde9fe5e62c3
SaraMoreiraG <sara_moreira@hotmail.es>
2023-08-09 18:32:15 +0200

    looking good

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScrollService } from 'src/app/services/scroll.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private scrollService: ScrollService) {}
+
   title = 'restaurante-el89';
+
+  scrollToBookingTable() {
+    this.scrollService.scrollTo('booking-table');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CardCarouselComponent } from './card-carousel/card-carousel.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CardCarouselComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
