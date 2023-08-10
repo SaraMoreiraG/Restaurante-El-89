6795ff328ef34d0e2274c50f44d64b2af91fa9bc
SaraMoreiraG <sara_moreira@hotmail.es>
2023-08-10 16:24:13 +0200

    adding scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { ScrollService } from 'src/app/services/scroll.service';
 
 @Component({
@@ -10,6 +10,20 @@ export class AppComponent {
   constructor(private scrollService: ScrollService) {}
 
   title = 'restaurante-el89';
+
+  hasScrolledPastHome = false;
+
+  @HostListener('window:scroll', ['$event'])
+
+  onScroll(event: Event) {
+    const scrollPosition = window.scrollY;
+
+    // Calculate the threshold based on the height of the "home" section
+    const threshold = window.innerHeight - 750;
+
+    // Check if the scroll position is beyond the threshold
+    this.hasScrolledPastHome = scrollPosition > threshold;
+  }
 
   scrollToBookingTable() {
     this.scrollService.scrollTo('booking-table');
